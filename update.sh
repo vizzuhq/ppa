5c3e5aced5b50e34a85c107d0183dc4283c1ef21
Laszlo Simon <laszlo.simon@gmail.com>
2021-03-01 16:19:15 +0100

    update script added

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+apt-ftparchive release . > Release
+gpg --default-key "simon@vizzuhq.com" -abs -o - Release > Release.gpg
+gpg --default-key "simon@vizzuhq.com" --clearsign -o - Release > InRelease
+
+git add -A
+git commit -m update
+git push
